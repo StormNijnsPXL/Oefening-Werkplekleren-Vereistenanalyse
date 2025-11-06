f55a54ba79ca538869320fd9032ad4f243394668
StormNijnsPXL <12502139@student.pxl.be>
2025-11-06 13:34:52 +0100

    DEMO-04

--- VereistenAnalyse/VereistenAnalyse/Program.cs
+++ VereistenAnalyse/VereistenAnalyse/Program.cs
@@ -6,11 +6,22 @@
         {
             const string PASSWORD = "1234!";
             const string USERNAME = "ADMIN";
+            int attempt = 0;
 
-            Console.Write("Gebruikersnaam: ");
-            string inputUsername = Console.ReadLine();
-            Console.Write("Wachtwoord: ");
-            string inputPassword = Console.ReadLine();
+            while (attempt < 3)
+            {
+                attempt++;
+                Console.Write("Gebruikersnaam: ");
+                string inputUsername = Console.ReadLine();
+                Console.Write("Wachtwoord: ");
+                string inputPassword = Console.ReadLine();
+
+                if (!inputUsername.Equals(USERNAME) || !inputPassword.Equals(PASSWORD))
+                {
+                    Console.WriteLine($"Foutieve login.\n{3 - attempt} pogingen over.");
+                }
+            }
+
         }
     }
 }

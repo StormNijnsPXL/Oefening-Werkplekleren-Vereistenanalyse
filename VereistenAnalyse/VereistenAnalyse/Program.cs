40015f8d58938a6fe9d4d464618812b262a859b2
StormNijnsPXL <12502139@student.pxl.be>
2025-11-06 13:29:18 +0100

    DEMO-03

--- VereistenAnalyse/VereistenAnalyse/Program.cs
+++ VereistenAnalyse/VereistenAnalyse/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            const string PASSWORD = "1234!";
+            const string USERNAME = "ADMIN";
+
             Console.Write("Gebruikersnaam: ");
             string inputUsername = Console.ReadLine();
             Console.Write("Wachtwoord: ");
